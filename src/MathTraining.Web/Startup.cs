6862b80c3a52b4353d880e75020be84fd699dc3c
Hadwin Jiang <hadwinjiang@126.com>
2017-01-20 13:47:17 +0800

    Add Seeder

--- src/MathTraining.Web/Startup.cs
+++ src/MathTraining.Web/Startup.cs
@@ -83,6 +83,8 @@ namespace MathTraining.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.Initialize(app.ApplicationServices);
         }
     }
 }
