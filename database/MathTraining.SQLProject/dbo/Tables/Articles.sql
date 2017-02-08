1e9dc7b2d935a2ba8a6dd86e4563b8d0b4e7ea92
Hadwin Jiang <hadwinjiang@126.com>
2017-02-08 17:35:33 +0800

    Added Articles sql

---
+++ database/MathTraining.SQLProject/dbo/Tables/Articles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Articles] (
+    [ArticleId]         UNIQUEIDENTIFIER NOT NULL,
+    [BriefIntroduction] NVARCHAR (256)   NOT NULL,
+    [ChineseTitle]      NVARCHAR (50)    NOT NULL,
+    [Created]           DATETIME2 (7)    NOT NULL,
+    [EnglishTitle]      NVARCHAR (50)    NOT NULL,
+    [FileNameOfView]    NVARCHAR (50)    NOT NULL
+);
